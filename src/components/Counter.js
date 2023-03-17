d03dff634c775dfd469cf4c943fef887c8c79f47
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2023-03-17 18:16:42 +0300

    Added get actions in component.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatchFunction = useDispatch();
   const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatchFunction({ type: "INCREMENT" });
+  };
+  const decrementHandler = () => {
+    dispatchFunction({ type: "DECREMENT" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Счётчик</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={decrementHandler}>-</button>
+      </div>
       <button onClick={toggleCounterHandler}>Спрятать / Показать</button>
     </main>
   );
